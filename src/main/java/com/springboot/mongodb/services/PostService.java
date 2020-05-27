4308ac58e1f081aa27f2066d7d4b640ac3289ac0
Gabriela Cordova <gabrielacordovaa@hotmail.com>
2020-05-27 17:48:41 -0300

    consulta simples com query methods

--- src/main/java/com/springboot/mongodb/services/PostService.java
+++ src/main/java/com/springboot/mongodb/services/PostService.java
@@ -1,5 +1,6 @@
 package com.springboot.mongodb.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,7 @@ public class PostService {
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public List<Post> findByTitlee(String text){
+		return postRepository.findByTitleContaining(text);
+	}
 }
