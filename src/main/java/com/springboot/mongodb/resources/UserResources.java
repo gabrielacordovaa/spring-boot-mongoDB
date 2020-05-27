492f93ecf7061b3ed786ab2f428f7e377cb94eb5
Gabriela Cordova <gabrielacordovaa@hotmail.com>
2020-05-27 14:52:21 -0300

    Usando padrao DTO

--- src/main/java/com/springboot/mongodb/resources/UserResources.java
+++ src/main/java/com/springboot/mongodb/resources/UserResources.java
@@ -1,8 +1,8 @@
 package com.springboot.mongodb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.mongodb.DTO.UserDTO;
 import com.springboot.mongodb.domain.User;
 import com.springboot.mongodb.services.UserService;
 
@@ -21,10 +22,11 @@ public class UserResources {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 
 }
