ac8d2db37f7c1eff5114d11f4fcbb64a8a01e0c9
Gabriela Cordova <gabrielacordovaa@hotmail.com>
2020-05-27 15:25:31 -0300

    insercao de usuario com post

--- src/main/java/com/springboot/mongodb/services/UserService.java
+++ src/main/java/com/springboot/mongodb/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.springboot.mongodb.DTO.UserDTO;
 import com.springboot.mongodb.domain.User;
 import com.springboot.mongodb.repositories.UserRepository;
 import com.springboot.mongodb.services.exception.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id){
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
